2139da49fa0db1ad25dbc3ffbfe179fbeedd3761
Adam Trojnar <atrojnar@kontomierz.pl>
2014-11-27 15:10:01 +0100

    init commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import org.xml.sax.SAXException;
+
+import java.io.IOException;
+
+public class Main {
+  public static void main( String[] params ) throws IOException, SAXException {
+    new Scraper();
+  }
+}
+
